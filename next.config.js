19d7a1271b0011007a50be3a3abe01766c07863b
Julian Camilo <julian20141016@gmail.com>
2022-02-05 17:27:34 -0500

    Deleted custom sass package

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const path = require("path");
-const withSass = require("@zeit/next-sass");
 
-module.exports = withSass({
-  cssModules: true,
-});
+
 
 module.exports = {
   reactStrictMode: true,
