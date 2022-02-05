03c7beae062b0b1df330ae015558c3decda18dd4
Julian Camilo <julian20141016@gmail.com>
2022-02-05 13:00:49 -0500

    Added Firebase global providers for the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,45 @@
 import React from "react";
 
+import {
+  FirebaseAppProvider,
+  FirestoreProvider,
+  useFirebaseApp,
+} from "reactfire";
+import { getFirestore } from "firebase/firestore";
 import { IconContext } from "react-icons";
 
-import "../styles/globals.scss";
+import firebaseConfig from "../services/firebaseConfig";
+
 import "antd/dist/antd.css";
+import "../styles/globals.scss";
+
+function FirebaseGlobalProvider(props) {
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      {props.children}
+    </FirebaseAppProvider>
+  );
+}
+
+function FirebaseSDKProvider(props) {
+  const firestoneInstance = getFirestore(useFirebaseApp());
+
+  return (
+    <FirestoreProvider sdk={firestoneInstance}>
+      {props.children}
+    </FirestoreProvider>
+  );
+}
 
 function Linguae({ Component, pageProps }) {
   return (
-    <IconContext.Provider value={{ size: "23" }}>
-      <Component {...pageProps} />
-    </IconContext.Provider>
+    <FirebaseGlobalProvider>
+      <FirebaseSDKProvider>
+        <IconContext.Provider value={{ size: "23" }}>
+          <Component {...pageProps} />
+        </IconContext.Provider>
+      </FirebaseSDKProvider>
+    </FirebaseGlobalProvider>
   );
 }
 
---
+++ services/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBoceq4BK49zBrzcIG-5RZekNG4bu2Ckjk",
+  authDomain: "linguae-a0c8d.firebaseapp.com",
+  projectId: "linguae-a0c8d",
+  storageBucket: "linguae-a0c8d.appspot.com",
+  messagingSenderId: "459795792938",
+  appId: "1:459795792938:web:09ebbf419a9fc525002f53",
+};
+
+export default firebaseConfig;
