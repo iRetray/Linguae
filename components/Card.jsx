5a70815a7f608b2f6c4a49291302235e9995c965
Rose <05rosa04@gmail.com>
2022-03-23 02:35:04 -0500

    Styles in cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,15 +5,16 @@ import { useRouter } from "next/router";
 
 /* import copy from "copy-to-clipboard"; */
 import { Tooltip } from "antd";
+import { HiOutlineClipboardCopy } from "react-icons/hi";
 
 export function Card({ id, englishValue, spanishValue, image, owner, avatar }) {
   const router = useRouter();
   const [textCopy, setTextCopy] = useState("Click to copy!");
 
-  /* const copyToWord = () => {
-    copy(englishValue);
+  const copyToWord = () => {
+    navigator.clipboard.writeText(englishValue);
     setTextCopy("Word copied!");
-  }; */
+  };
 
   const goToCardPage = () => {
     router.replace({
@@ -23,50 +24,52 @@ export function Card({ id, englishValue, spanishValue, image, owner, avatar }) {
   };
 
   return (
-    <Tooltip title={textCopy} color="purple">
-      <div
-        className="CardContainer"
-        onClick={goToCardPage}
-        onMouseOver={() => {
-          setTextCopy("Click to copy!");
-        }}
-      >
+    <div className="cardContainer">
+      <div className="imageCard" onClick={goToCardPage}>
+        <Image
+          src={`/api/imageProxy?url=${encodeURIComponent(image)}`}
+          width="800px"
+          height="800px"
+          objectFit="cover"
+          alt="Related word image"
+        />
+      </div>
+
+      <div className="informationContainer">
+        <div className="miniCardContainer">
+          <Tooltip title={textCopy} color="purple">
+            <div
+              className="wordContainer"
+              onMouseOver={() => {
+                setTextCopy("Click to copy!");
+              }}
+              onClick={copyToWord}
+            >
+              <span>{englishValue}</span>
+              <HiOutlineClipboardCopy />
+            </div>
+          </Tooltip>
+          <span className="definition">{spanishValue}</span>
+        </div>
+
         {owner && (
           <div className="ownerSection">
             <div className="avatarImage">
-              <Image
-                loading="eager"
-                src={`/api/imageProxy?url=${encodeURIComponent(avatar)}`}
-                width="30px"
-                height="30px"
-                objectFit="cover"
-                alt="Related word image"
-              />
+              <Tooltip title={owner}>
+                <Image
+                  loading="eager"
+                  src={`/api/imageProxy?url=${encodeURIComponent(avatar)}`}
+                  width="30px"
+                  height="30px"
+                  objectFit="cover"
+                  alt="Related word image"
+                />
+              </Tooltip>
             </div>
-            <p className="ownerName">
-              <p>Created by</p>
-              <p>
-                <strong>{owner}</strong>
-              </p>
-            </p>
+            <div className="ownerName" onClick={copyToWord}></div>
           </div>
         )}
-        <div className="informationContainer">
-          <div className="miniCardContainer">
-            <span className="wordContainer">{englishValue}</span>
-            <span className="definition">{spanishValue}</span>
-          </div>
-        </div>
-        <div className="imageCard">
-          <Image
-            src={`/api/imageProxy?url=${encodeURIComponent(image)}`}
-            width="800px"
-            height="800px"
-            objectFit="cover"
-            alt="Related word image"
-          />
-        </div>
       </div>
-    </Tooltip>
+    </div>
   );
 }
